2b7f192dd498f8aca0065c27c587b5d566452cc2
Erika Bailon <erikabailon@cu-engr2-1-16-10.201.62.149.int.colorado.edu>
2019-04-19 11:46:18 -0600

    update

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("home.html")
-  
- @app.route("/Salvador")
- def salvador():
-  return "Hello, Salvador"
- 
- if _name_ == "_main_":
-  app.run(debug=True)
-  We made two new changes
- 
+    return render_template("home.html")
+    
+@app.route("/about)
+def about():
+    return render_template("about.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
