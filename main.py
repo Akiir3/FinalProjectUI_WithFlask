15c80940a20d1de2d0ab281f85413c5c661aca92
Akiir3 <erba0445@colorado.edu>
2019-04-21 15:47:24 -0600

    update

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 
+#This is for flask to know where to look for, ___ is used in python
 app = Flask(__name__)
 
+#decorator that allows us to write the function to return the website
 @app.route("/")
 def home():
     return render_template("home.html")
     
-@app.route("/about)
+@app.route("/about") 
 def about():
     return render_template("about.html")
     
