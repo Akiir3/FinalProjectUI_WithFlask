00289d7cb329f3338104cb07b1e1c45663f1746a
Akiir3 <erba0445@colorado.edu>
2019-04-21 17:27:03 -0600

    update

--- main.py
+++ main.py
@@ -3,15 +3,29 @@ from flask import Flask, render_template
 #This is for flask to know where to look for, ___ is used in python
 app = Flask(__name__)
 
+#Dummy data for users 
+DayAct = [
+	{
+		'user': "User1",
+		'8am': "Breakfast",
+		'10pm': "Dinner"
+	},
+	{
+		'user': "User1",
+		'8am': "Breakfast",
+		'10pm': "Dinner"
+	}
+]
+
 #decorator that allows us to write the function to return the website
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", DayAct=DayAct)
     
 @app.route("/about") 
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="about")
     
 #This conditional is true if we run the script directly
 if __name__ == "__main__":
